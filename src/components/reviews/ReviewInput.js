613446512bf30a38dcaca02222b12e27045a56af
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-12-09 05:10:32 -0500

    commit tree 5a3a362a551817eb7b6e3e4748f9e5098de0b1c6

--- src/components/reviews/ReviewInput.js
+++ src/components/reviews/ReviewInput.js
@@ -2,10 +2,37 @@ import React, { Component } from 'react';
 import Reviews from './Reviews';
 
 class ReviewInput extends Component {
+
+	state = {
+		text: ''
+	}
+
+	handleOnChange = event => {
+		this.setState({
+			text: event.target.value
+		})
+	}
+
+	handleOnSubmit = event => {
+		event.preventDefault()
+		this.props.addReview({ text: this.state.text, restaurantId: this.props.restaurantId})
+		this.setState({
+			text: ''
+		})
+	}
+
   render() {
     return (
       <div>
-        Review Input
+        <form onSubmit={this.handleOnSubmit}>
+					<input
+						type="text"
+						name="text"
+						onChange={this.handleOnChange}
+						value={this.state.text}
+					/>
+					<input type="submit"/>
+				</form>
       </div>
     );
   }
